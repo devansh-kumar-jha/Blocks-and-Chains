8ed32bb583d6ca07f5544bd624653d9ccc41550d
Devansh Kumar Jha <77242838+dkjha20@users.noreply.github.com>
2021-05-16 22:35:58 +0530

    Update second1.js

--- second1.js
+++ second1.js
@@ -1,15 +1,13 @@
 const crypto = require("crypto")
+const fs = require('fs');
 
-// The `generateKeyPairSync` method accepts two arguments:
-// 1. The type ok keys we want, which in this case is "rsa"
-// 2. An object with the properties of the key
-const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+
+var { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
 	// The standard secure default length for RSA keys is 2048 bits
 	modulusLength: 2048,
 })
 
-// This is the data we want to encrypt
-const data = "my secret data to be kept safe"
+const data = 'my secret data'
 
 const encryptedData = crypto.publicEncrypt(
 	{
@@ -21,43 +19,39 @@ const encryptedData = crypto.publicEncrypt(
 	Buffer.from(data)
 )
 
-// The encrypted data is in the form of bytes, so we print it in base64 format
-// so that it's displayed in a more readable form
-console.log("encypted data: ", encryptedData.toString("base64"))
+
+console.log("encypted data: ", encryptedData.toString('hex'))
 
 const decryptedData = crypto.privateDecrypt(
 	{
 		key: privateKey,
-		// In order to decrypt the data, we need to specify the
-		// same hashing function and padding scheme that we used to
-		// encrypt the data in the previous step
 		padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
 		oaepHash: "sha256",
 	},
 	encryptedData
 )
 
-// The decrypted data is of the Buffer type, which we can convert to a
-// string to reveal the original data
 console.log("decrypted data: ", decryptedData.toString())
 
-// Create some sample data that we want to sign
-const verifiableData = "this need to be verified"
+fs.openSync("./Keys/public.pem","r");
+publicKey = fs.readFileSync("./Keys/public.pem",'utf8');
 
-// The signature method takes the data we want to sign, the
-// hashing algorithm, and the padding scheme, and generates
-// a signature in the form of bytes
+fs.openSync("./Keys/private.pem","r");
+privateKey = fs.readFileSync("./Keys/private.pem",'utf8');
+
+fs.openSync("./Data/un-encrypted.txt","r");
+var verifiableData = fs.readFileSync("./Data/un-encrypted.txt",'utf8');
+
+console.log(verifiableData.toString())
+
 const signature = crypto.sign("sha256", Buffer.from(verifiableData), {
 	key: privateKey,
 	padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
 })
 
-console.log(signature.toString("base64"))
+console.log(signature.toString('hex'))
 
-// To verify the data, we provide the same hashing algorithm and
-// padding scheme we provided to generate the signature, along
-// with the signature itself, the data that we want to
-// verify against the signature, and the public key
+
 const isVerified = crypto.verify(
 	"sha256",
 	Buffer.from(verifiableData),
@@ -68,6 +62,5 @@ const isVerified = crypto.verify(
 	signature
 )
 
-// isVerified should be `true` if the signature is valid
 console.log("signature verified: ", isVerified)
 
